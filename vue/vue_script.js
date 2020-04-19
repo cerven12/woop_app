26630a25e4c7edff6ef1eed040e02341a2972bbd
takuto <pian.tt8.gb4@gmail.com>
2020-04-19 14:52:08 +0900

    modify axios.put

--- vue/vue_script.js
+++ vue/vue_script.js
@@ -1,17 +1,20 @@
 new Vue({
     el: '#app',
-    data: {
-        goals: {},
-        new_goal: '',
-    },
-    mounted() {
+    data: function (){
+        return {
+            goals: {},
+            new_goal: '',
+            isEditGoal: false,
+    }
+},
+    created() {
         this.reloadGoal();
     },
     methods: {
-        postGoal: function () {
+        postGoal: function (title) {
             const vm = this;
             axios.post('http://127.0.0.1:8000/api/v1/Goal/',
-                { title: this.new_goal })
+                { title: title })
                 .then(response => { vm.reloadGoal(); })
         },
         deleteGoal: function (id) {
@@ -19,17 +22,20 @@ new Vue({
             axios.delete('http://127.0.0.1:8000/api/v1/Goal/' + id)
                 .then(response => { vm.reloadGoal(); })
         },
-        updataGoal: function (id) {
+        updateGoal: function (id, title) {
             const vm = this;
-            axios.put('http://127.0.0.1:8000/api/v1/Goal/' + id,
-                { title: this.input_title })
+            console.log(vm)
+            axios.put('http://127.0.0.1:8000/api/v1/Goal/' + id + '/',
+                { id: id, title: title })
+                .then(response => { vm.isEditGoal = false })
+                .catch((error) =>{ console.log(error) })
                 .then(response => { vm.reloadGoal(); })
         },
         reloadGoal() {
             const vm = this;
             axios.get('http://127.0.0.1:8000/api/v1/Goal/')
                 .then((response) => { vm.goals = response.data })
-        }
+        },
     },
 }
 )
