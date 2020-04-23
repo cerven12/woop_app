10e5037de4453c0469a9fef5a66821a9d6b1de63
takuto <pian.tt8.gb4@gmail.com>
2020-04-23 23:02:22 +0900

    Add a vue cli. A page for adding `Goal model` was created with vuetify.

---
+++ frontend/src/App.vue
@@ -0,0 +1,19 @@
+<template>
+<NewGoal></NewGoal>
+</template>
+
+<script>
+import NewGoal from './components/NewGoal';
+
+export default {
+  name: 'App',
+
+  components: {
+    NewGoal,
+  },
+
+  data: () => ({
+    //
+  }),
+};
+</script>
