4324f24a69e8cb6e8b4775568ac8c78ad3ed4893
takuto <pian.tt8.gb4@gmail.com>
2020-04-28 15:32:56 +0900

    A child model of the goal model can be defined and CRUD can be performed.

--- apiv1/serializers.py
+++ apiv1/serializers.py
@@ -1,46 +1,61 @@
 from rest_framework import serializers
-from woop.models import Goal, Task, Motive, SelfTranscendenceGoal, FutureSelf, Worry, Idea
+from woop.models import Goal, Task, Motive, SelfTranscendenceGoal, FutureSelf, Worry, Idea, Reference, Note
 from drf_writable_nested.serializers import WritableNestedModelSerializer
 
 
 class TaskSerializer(serializers.ModelSerializer):
-
     class Meta:
         model = Task
-        fields = ['id', 'task_title', 'created_at', 'goal']
+        fields = ['task_id', 'task_title', 'created_at', 'goal']
 
 
 class MotiveSerializer(serializers.ModelSerializer):
-
     class Meta:
         model = Motive
-        fields = ['id', 'motive', 'created_at', 'goal']
+        fields = ['motive_id', 'motive', 'created_at', 'goal']
 
 class SelfTranscendenceGoalSerializer(serializers.ModelSerializer):
-
     class Meta:
         model = SelfTranscendenceGoal
-        fields = ['id', 'self_transcendence_goal', 'created_at', 'goal']
+        fields = ['selftrans_id', 'self_transcendence_goal', 'created_at', 'goal']
+
 
 class FutureSelfSerializer(serializers.ModelSerializer):
-
     class Meta:
         model = FutureSelf
-        fields = ['id', 'future_self', 'created_at', 'goal']
+        fields = ['future_id', 'future_self', 'created_at', 'goal']
+
 
 class IdeaSerializer(serializers.ModelSerializer):
-
     class Meta:
-        model = Worry
-        fields = ['id', 'idea', 'created_at', 'worry']
+        model = Idea
+        fields = ['idea_id', 'idea', 'created_at', 'worry']
 
 
 class WorrySerializer(WritableNestedModelSerializer):
-    ideas = IdeaSerializer(many=True, required=False)
+    ideas = IdeaSerializer(many=True, required=False, allow_null=True)
 
     class Meta:
         model = Worry
-        fields = ['id', 'worry', 'created_at', 'goal', 'ideas']
+        fields = ['worry_id', 'worry', 'created_at', 'goal', 'ideas']
+
+
+class ReferenceSerializer(WritableNestedModelSerializer):
+    class Meta:
+        model = Reference
+        fields = [
+            'reference_id', 'reference_name', 'reference_use',
+            'reference_source', 'created_at', 'goal'
+            ]
+
+
+class NoteSerializer(WritableNestedModelSerializer):
+    class Meta:
+        model = Note
+        fields = [
+            'note_id', 'note_title', 'note_main',
+            'created_at', 'update_at', 'goal'
+            ]
 
 
 class GoalSerializer(WritableNestedModelSerializer):
@@ -48,12 +63,17 @@ class GoalSerializer(WritableNestedModelSerializer):
     motives = MotiveSerializer(many=True, required=False)
     self_transcendence_goals = SelfTranscendenceGoalSerializer(many=True, required=False)
     future_selves = FutureSelfSerializer(many=True, required=False)
-    worries = WorrySerializer(many=True, required=False)
+    worries = WorrySerializer(many=True, required=False, allow_null=True)
+    references = ReferenceSerializer(many=True, required=False)
+    notes = NoteSerializer(many=True, required=False)
 
     class Meta:
         model = Goal
-        fields = ['id', 'goal_title','created_at','goal_description',
-        'start_date','deadline','achievement','progress_type',
-        'motives', 'self_transcendence_goals', 'future_selves', 'worries', 'tasks']
+        fields = [
+            'goal_id', 'goal_title','created_at','goal_description',
+            'start_date','deadline','achievement','progress_type',
+            'motives', 'self_transcendence_goals', 'future_selves', 'worries',
+            'references', 'notes', 'tasks'
+            ]
 
 
--- apiv1/views.py
+++ apiv1/views.py
@@ -1,35 +1,84 @@
 from django.shortcuts import render
-from rest_framework import viewsets
-# models
-from woop.models import Goal, Task, Motive, SelfTranscendenceGoal, FutureSelf, Worry , Idea
-from .serializers import GoalSerializer, TaskSerializer, MotiveSerializer, SelfTranscendenceGoalSerializer, FutureSelfSerializer, WorrySerializer, IdeaSerializer
+from rest_framework import viewsets, generics
+from rest_framework.response import Response
+
+from woop.models import Goal, Task, Motive, SelfTranscendenceGoal, FutureSelf, Worry , Idea, Reference, Note
+from .serializers import GoalSerializer, TaskSerializer, MotiveSerializer, SelfTranscendenceGoalSerializer, FutureSelfSerializer, WorrySerializer, IdeaSerializer, ReferenceSerializer, NoteSerializer
 
 
-class GoalViewSet(viewsets.ModelViewSet):
+class GoalViewSet(viewsets.ViewSet, generics.ListCreateAPIView, generics.RetrieveUpdateDestroyAPIView):
     queryset = Goal.objects.all()
     serializer_class = GoalSerializer
+
+    def list(self, request,):
+        queryset = Goal.objects.filter()
+        serializer = GoalSerializer(queryset, many=True)
+        return Response(serializer.data)
+
+    def retrieve(self, request, pk=None):
+        queryset = Goal.objects.filter()
+        client = generics.get_object_or_404(queryset, pk=pk)
+        serializer = GoalSerializer(client)
+        return Response(serializer.data)
 
 
 class TaskViewSet(viewsets.ModelViewSet):
     queryset = Task.objects.all()
     serializer_class = TaskSerializer
 
+
 class MotiveViewSet(viewsets.ModelViewSet):
     queryset = Motive.objects.all()
     serializer_class = MotiveSerializer
+
 
 class SelfTranscendenceGoalViewSet(viewsets.ModelViewSet):
     queryset = SelfTranscendenceGoal.objects.all()
     serializer_class = SelfTranscendenceGoalSerializer
 
+
 class FutureSelfViewSet(viewsets.ModelViewSet):
     queryset = FutureSelf.objects.all()
     serializer_class = FutureSelfSerializer
 
-class WorrySelfViewSet(viewsets.ModelViewSet):
+
+class WorryViewSet(viewsets.ViewSet, generics.ListCreateAPIView, generics.RetrieveUpdateDestroyAPIView):
     queryset = Worry.objects.all()
     serializer_class = WorrySerializer
 
-class IdeaSelfViewSet(viewsets.ModelViewSet):
+    def list(self, request, goal_pk=None):
+        queryset = Worry.objects.filter(goal_id=goal_pk)
+        serializer = WorrySerializer(queryset, many=True)
+        return Response(serializer.data)
+
+    def retrieve(self, request, pk=None, goal_pk=None):
+        queryset = Worry.objects.filter(pk=pk, goal_id=goal_pk)
+        worry = generics.get_object_or_404(queryset, pk=pk)
+        serializer = WorrySerializer(worry)
+        return Response(serializer.data)
+
+
+class IdeaViewSet(viewsets.ViewSet, generics.ListCreateAPIView, generics.RetrieveUpdateDestroyAPIView):
     queryset = Idea.objects.all()
     serializer_class = IdeaSerializer
+
+    def list(self, request, goal_pk=None, worry_pk=None):
+        queryset = Idea.objects.filter(worry_id__goal_id=goal_pk, worry_id=worry_pk)
+        serializer = IdeaSerializer(queryset, many=True)
+        return Response(serializer.data)
+
+    def retrieve(self, request, pk=None, goal_pk=None, worry_pk=None):
+        queryset = Idea.objects.filter(pk=pk, worry_id__goal_id=goal_pk, worry_id=worry_pk)
+        idea = generics.get_object_or_404(queryset, pk=pk)
+        serializer = IdeaSerializer(idea)
+        return Response(serializer.data)
+
+
+class ReferenceViewSet(viewsets.ModelViewSet):
+    queryset = Reference.objects.all()
+    serializer_class = ReferenceSerializer
+
+
+class NoteViewSet(viewsets.ModelViewSet):
+    queryset = Note.objects.all()
+    serializer_class = NoteSerializer
