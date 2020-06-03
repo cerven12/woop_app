4c3df2f76a4d5099087134863f2a02482b87b12b
takuto <pian.tt8.gb4@gmail.com>
2020-06-03 23:01:48 +0900

    Change settings of each fields of CusomUserModel.

--- account/admin.py
+++ account/admin.py
@@ -3,11 +3,24 @@ from django.contrib.auth.models import Group
 from django.contrib.auth.admin import UserAdmin
 from django.contrib.auth.forms import ReadOnlyPasswordHashField
 from django.utils.translation import ugettext_lazy as _
+from django.contrib.auth.forms import UserChangeForm, UserCreationForm
+
 
 from .models import CustomUser
 
+class MyUserChangeForm(UserChangeForm):
+    class Meta:
+        model = CustomUser
+        fields = '__all__'
+
+class MyUserCreationForm(UserCreationForm):
+    class Meta:
+        model = CustomUser
+        fields = ('email',)
+
 class CustomUserAdmin(UserAdmin):
     fieldsets = (
+        # index 0 show change form for field.
         (None, {'fields': ('email', 'password',)}),
         (_('Personal info'), {'fields': ('username',)}),
         (_('Permissions'), {'fields': ('is_active', 'is_staff', 'is_superuser',
@@ -20,9 +33,11 @@ class CustomUserAdmin(UserAdmin):
             'fields': ('email', 'password1', 'password2'),
         }),
     )
-    list_display = ('email', 'username',  'is_staff',)
+    form = MyUserChangeForm
+    add_form = MyUserCreationForm
+    list_display = ('email', 'username',  'is_staff','user_id',)
     list_filter = ('is_staff', 'is_superuser', 'is_active', 'groups',)
-    search_fields = ('email', )
+    search_fields = ('email', 'username','user_id')
     ordering = ('email',)
 admin.site.register(CustomUser, CustomUserAdmin)
 
--- account/models.py
+++ account/models.py
@@ -6,14 +6,12 @@ from django.utils import timezone
 from django.utils.translation import gettext_lazy as _
 from django.core.mail import send_mail
 from django.contrib.auth.base_user import BaseUserManager
+import uuid
 
 class UserManager(BaseUserManager):
     use_in_migrations = True
 
     def _create_user(self, email, password, **extra_fields):
-        """
-        Create and save a user with the given username, email, and password.
-        """
         if not email:
             raise ValueError('The given email must be set')
         email = self.normalize_email(email)
@@ -41,19 +39,17 @@ class UserManager(BaseUserManager):
 class CustomUser(AbstractBaseUser, PermissionsMixin):
     # Change Validator UNICODE -> ASCII
     username_validator = ASCIIUsernameValidator()
-    # uuid = models.UUIDField(default=uuid_lib.uuid4,
-                            # primary_key=True, editable=False)
+    user_id = models.UUIDField(default=uuid.uuid4,
+                            primary_key=True, editable=False)
+    # username is just display.
     username = models.CharField(
         _('username'),
         max_length=150,
-        unique=True,
         help_text=_('Required. 150 characters or fewer. Letters, digits and @/./+/-/_ only.'),
         validators=[username_validator],
-        error_messages={
-            'unique': _("A user with that username already exists."),
-        },
+        error_messages={'unique': _("A user with that username already exists."),},
     )
-    email = models.EmailField(_('email address'), blank=True)
+    email = models.EmailField(_('email address'), unique=True)
     # profile_icon = models.ImageField(_('profile icon'), upload_to='profile_icons', null=True, blank=True)
     is_staff = models.BooleanField(
         _('staff status'),
@@ -73,28 +69,19 @@ class CustomUser(AbstractBaseUser, PermissionsMixin):
     objects = UserManager()
 
     EMAIL_FIELD = 'email'
-    USERNAME_FIELD = 'username'
-    REQUIRED_FIELDS = ['email']
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['username']
 
     class Meta:
         verbose_name = _('user')
         verbose_name_plural = _('users')
-        #abstract = True # ここを削除しないといけないことを忘れない！！！！！！！！！！
 
     def clean(self):
         super().clean()
         self.email = self.__class__.objects.normalize_email(self.email)
 
-    # def get_full_name(self):
-    #     """
-    #     Return the first_name plus the last_name, with a space in between.
-    #     """
-    #     full_name = '%s %s' % (self.first_name, self.last_name)
-    #     return full_name.strip()
-    #
-    # def get_short_name(self):
-    #     """Return the short name for the user."""
-    #     return self.first_name
+    def get_username(self):
+        return self.username
 
     def email_user(self, subject, message, from_email=None, **kwargs):
         """Send an email to this user."""
