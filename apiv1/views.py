932a207540714ee392c2ab0c7730214b4c30cd0b
takuto <pian.tt8.gb4@gmail.com>
2020-06-04 22:08:56 +0900

    Revise of nested url and views.py

--- apiv1/views.py
+++ apiv1/views.py
@@ -3,65 +3,65 @@ from rest_framework import viewsets, generics
 from rest_framework.response import Response
 
 from woop.models import Goal, Task, Motive, SelfTranscendenceGoal, FutureSelf, Worry, Idea, Reference, Note
-from .serializers import GoalSerializer, TaskSerializer, MotiveSerializer, SelfTranscendenceGoalSerializer, FutureSelfSerializer, WorrySerializer, IdeaSerializer, ReferenceSerializer, NoteSerializer
+from .serializers import GoalSerializer, TaskSerializer, MotiveSerializer, SelfTranscendenceGoalSerializer, \
+    FutureSelfSerializer, WorrySerializer, IdeaSerializer, ReferenceSerializer, NoteSerializer
 
 # permission setting
 from rest_framework.permissions import IsAuthenticated
-
-# About the
+from rest_framework_extensions.mixins import NestedViewSetMixin
 
 
 #  About the Goal Model
-class GoalViewSet(viewsets.ModelViewSet):
+class GoalViewSet(NestedViewSetMixin, viewsets.ModelViewSet):
     'add "return self.queryset.filter(user=self.request.user)"'
     queryset = Goal.objects.all()
     serializer_class = GoalSerializer
     permission_classes = [IsAuthenticated]  # authenticated-accessible
 
 
-class TaskViewSet(viewsets.ModelViewSet):
+class TaskViewSet(NestedViewSetMixin, viewsets.ModelViewSet):
     queryset = Task.objects.all()
     serializer_class = TaskSerializer
     permission_classes = [IsAuthenticated]
 
 
-class MotiveViewSet(viewsets.ModelViewSet):
+class MotiveViewSet(NestedViewSetMixin, viewsets.ModelViewSet):
     queryset = Motive.objects.all()
     serializer_class = MotiveSerializer
     permission_classes = [IsAuthenticated]
 
 
-class SelfTranscendenceGoalViewSet(viewsets.ModelViewSet):
+class SelfTranscendenceGoalViewSet(NestedViewSetMixin, viewsets.ModelViewSet):
     queryset = SelfTranscendenceGoal.objects.all()
     serializer_class = SelfTranscendenceGoalSerializer
     permission_classes = [IsAuthenticated]
 
 
-class FutureSelfViewSet(viewsets.ModelViewSet):
+class FutureSelfViewSet(NestedViewSetMixin, viewsets.ModelViewSet):
     queryset = FutureSelf.objects.all()
     serializer_class = FutureSelfSerializer
     permission_classes = [IsAuthenticated]
 
 
-class WorryViewSet(viewsets.ModelViewSet):
+class WorryViewSet(NestedViewSetMixin, viewsets.ModelViewSet):
     queryset = Worry.objects.all()
     serializer_class = WorrySerializer
     permission_classes = [IsAuthenticated]
 
 
-class IdeaViewSet(viewsets.ModelViewSet):
+class IdeaViewSet(NestedViewSetMixin, viewsets.ModelViewSet):
     queryset = Idea.objects.all()
     serializer_class = IdeaSerializer
     permission_classes = [IsAuthenticated]
 
 
-class ReferenceViewSet(viewsets.ModelViewSet):
+class ReferenceViewSet(NestedViewSetMixin, viewsets.ModelViewSet):
     queryset = Reference.objects.all()
     serializer_class = ReferenceSerializer
     permission_classes = [IsAuthenticated]
 
 
-class NoteViewSet(viewsets.ModelViewSet):
+class NoteViewSet(NestedViewSetMixin, viewsets.ModelViewSet):
     queryset = Note.objects.all()
     serializer_class = NoteSerializer
     permission_classes = [IsAuthenticated]
