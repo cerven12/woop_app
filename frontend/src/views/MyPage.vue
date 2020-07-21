7925c9f93077d6b8ad457b21cc4586cfe950ce1e
takuto <pian.tt8.gb4@gmail.com>
2020-07-21 21:07:19 +0900

    Enable dynamic page transitions based on the URL.

--- frontend/src/views/MyPage.vue
+++ frontend/src/views/MyPage.vue
@@ -2,21 +2,43 @@
   <div>
     <div id="css-grid">
       <div id="goals-area">
-        <h1>My Page</h1>
-        <pre style="font-size:10px;">{{ Goals }}</pre>
+        <v-container fluid>
+          <v-row>
+            <v-col cols="1"></v-col>
+            <div class="scrolling-wrapper">
+              <div class="circle">P</div>
+              <div class="circle">S</div>
+            </div>
+          </v-row>
 
-        <div v-for="(goal, index) in Goals" :key="index">
-          <nav>
-            <router-link
-              :to="{ name: 'goal', params: { id: goal.goal_id } }"
-              >{{ goal.goal_title }}</router-link
-            >
-          </nav>
-        </div>
+          <v-row justify="center">
+            <v-col cols="1"></v-col>
+            <v-col cols="10">
+              <h1 class="yourgoal">Your Goals<span>+</span></h1>
+            </v-col>
+          </v-row>
+
+          <v-row>
+            <v-col cols="2"></v-col>
+            <v-col cols="8">
+              <div v-for="(goal, index) in Goals" :key="index">
+                <nav>
+                  <router-link
+                    :to="{ name: 'goal', params: { id: goal.goal_id } }"
+                  >
+                    <div class="goals-list">
+                      {{ goal.goal_title }}
+                    </div>
+                  </router-link>
+                </nav>
+              </div>
+            </v-col>
+          </v-row>
+        </v-container>
       </div>
+      <div id="tasks-area"><v-container></v-container></div>
+      <div id="memos-area"><v-container></v-container></div>
     </div>
-    <div id="tasks-area">sas</div>
-    <div id="memos-area">adadff</div>
   </div>
 </template>
 
@@ -28,11 +50,9 @@ export default {
   data: function () {
     return {
       Goals: {},
-      test: ""
     };
   },
-  methods: {
-  },
+  methods: {},
   mounted: function () {
     let vm = this;
     // goalの小モデルの情報は取得しないAPIを作成し、それを使ったほうが省エネになりそう
@@ -43,26 +63,90 @@ export default {
 };
 </script>
 
-<style>
+<style scoped>
 /*   ------------------------------------------------------------
                             CSS Grid
  ------------------------------------------------------------*/
 #css-grid {
   display: grid;
   grid-template-rows: 1fr;
-  grid-template-columns: 800px 100px 100px;
+  grid-template-columns: 50% 25% 25%;
   grid-template-areas: "goals tasks memos";
 }
 
 #goals-area {
   grid-area: goals;
 }
+
 #tasks-area {
   grid-area: tasks;
-  background: blue;
+  background: #56ccf2;
+  box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
+  border-radius: 80px 0px 0px 80px;
+  position: relative;
+  width: 200%;
+  min-height:950px;
 }
+
 #memos-area {
   grid-area: memos;
-  background: yellow;
+  background: #2d9cdb;
+  box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
+  border-radius: 80px 0px 0px 80px;
+  position: relative;
+}
+
+.goals-list {
+  background: #e8e8e8;
+  color: #4465c0;
+  font-size: 20px;
+  border-radius: 80px;
+  width: 550px;
+  min-height: 70px;
+  padding: 10px 40px;
+  margin: 20px;
+    font-weight: 700;
+
+}
+
+a {
+  text-decoration: none;
+}
+
+.yourgoal {
+  font-family: Roboto;
+  font-style: normal;
+  font-weight: normal;
+  /* font-size: 34px; */
+  line-height: 40px;
+  color: #404040;
+  font-size: 34px;
+}
+
+.circle {
+  display: inline-block;
+  width: 80px;
+  height: 80px;
+  border-radius: 50%;
+  background: skyblue;
+  text-align: center;
+  line-height: 80px;
+  font-size: 32px;
+  color: #404040;
+  flex: 0 0 auto;
+  position: relative;
+  overflow: scroll;
+  margin: 20px;
+  box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
+}
+
+.scrolling-wrapper {
+  display: flex;
+  flex-wrap: nowrap;
+  overflow-x: auto;
+}
+span {
+  margin: 0px 15px;
+  color: #404040;
 }
 </style>
