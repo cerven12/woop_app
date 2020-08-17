e87581c6e056acf35a793054cdb8dca6f90887c4
takuto <pian.tt8.gb4@gmail.com>
2020-08-17 15:47:08 +0900

    delete board

--- frontend/src/views/goal/note/Note.vue
+++ frontend/src/views/goal/note/Note.vue
@@ -6,7 +6,7 @@
           <v-col cols="8" lg="8" md="9" sm="11">
             <h2 class="message-title">
               Note
-              <div style="display: inline;" @click="addNote">+</div>
+              <div style="display: inline;" @click="addNote"><v-btn depressed small rounded color="#4465c0" style="color: #f0f0f0;">add memo +</v-btn></div>
             </h2>
           </v-col>
         </v-row>
